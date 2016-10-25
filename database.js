756c097e225d8cfcf5f373e3d617f9f2f08d9681
Jordan Hood <jordan.hood@holidayextras.com>
2016-10-25 17:16:09 +0100

    finalised using ORM

--- database.js
+++ database.js
@@ -1,57 +1,87 @@
 'use strict';
 let orm = require("orm");
 let db;
+let Users;
+const util = require('util')
 module.exports = {
   setup: (connectionConfig, callback) => {
     // setup the database connection
     let opts = {
-    host: connectionConfig.host,
-    database: connectionConfig.database,
-    user: connectionConfig.user,
-    password: connectionConfig.password,
-    protocol: 'postgres',
-    query:    {pool: true}
+      host: connectionConfig.host,
+      database: connectionConfig.database,
+      user: connectionConfig.user,
+      password: connectionConfig.password,
+      protocol: 'postgres',
+      query:
+        {
+          pool: true
+        }
     };
     db = orm.connect(opts, (err, db) => {
     if(err) return callback(err, null);
+    db.settings.set("instance.autoSave", true);
+    Users = db.define('users', {
+      id: {type: 'serial', key:true}, 
+      email: {type: 'text'},
+      forename: {type: 'text'},
+      surename: {type: 'text'},
+      created_on: {type: 'text'},
+    }, {
+      methods :{
+        setSurename : function(surename){
+          this.surename = surename;
+        },
+        setForename: function(forename){
+          this.forename = forename;
+        },
+        setEmail: function(email){
+          this.email = email;
+        }
+      }
+    });
+
     return callback(null, true);
-   });
+    });
   },
-}
-//   getAllUsers: (callback) => {
-//     dbWrapper.fetchAll('SELECT * FROM users', null, (err, res) => {
-//       if (err) return callback(err);
-//       callback(null, res);
-//     });
-//   },
 
-//   search: (params, callback) => {
-//     dbWrapper.fetchAll('SELECT * FROM users WHERE forename LIKE ? AND surename LIKE ?', ['%' + params.forename + '%', '%' + params.surename + '%'], (err, result) => {
-//       if (err) return callback(err);
-//       callback(null, result);
-//     });
-//   },
+  getAllUsers: (callback) => {
+    Users.all((err, res) =>{
+        return callback(err, res);
+    })
+  },
+  search: (params, callback) => {
+    Users.find({forename : params.forename, surename: params.surename}, (err, res) => {
+      return callback(err, res);
+    })
+  },
 
-//   insertUser: (params, callback) => {
-//     let date = new Date();
-//     let userData = { email: params.email, forename: params.forename, surename: params.surename, created_on: date.toLocaleDateString() + ' ' + date.toLocaleTimeString() };
-//     dbWrapper.insert('users', userData, (err) => {
-//       if (err) return callback(err);
-//       callback(null, true);
-//     });
-//   },
+  insertUser: (params, callback) => {
+    let newRecord = {
+      forename: params.forename,
+      surename: params.surename,
+      email: params.email
+    };
+    Users.create(newRecord, (err, res) =>{
+      return callback(err, res);
+    })
+  },
 
-//   updateUser: (newData, oldData, callback) => {
-//     dbWrapper.update('users', newData, [['forename=?', oldData.forename], ['surename=?', oldData.surename]], (err, res) => {
-//       if (err) return callback(err);
-//       callback(null, res);
-//     });
-//   },
+  updateUser: (newData, oldData, callback) => {
+    Users.find({forename : oldData.forename, surename: oldData.surename},1, (err, person) => {
+      person = person[0]; //ditch the array it comes back in
+      person.email = newData.email;
+      person.forename = newData.forename;
+      person.surename = newData.surename; 
+      person.save((err) => {
+        return callback(err, true);
+      })
+    });
+  },
 
-//   deleteUser: (userToDelete, callback) => {
-//     dbWrapper.remove('users', [ ['forename=?', userToDelete.forename], ['surename=?', userToDelete.surename] ], (err, res) => {
-//       if (err) return callback(err);
-//       callback(null, res);
-//     });
-//   }
-// };
+  deleteUser: (userToDelete, callback) => {
+  Users.find({forename : userToDelete.forename, surename: userToDelete.surename},1, (err, person) => {
+    person = person[0]; //ditch the array it comes back in
+    person.remove(callback);
+  })
+  }
+};
--- server.js
+++ server.js
@@ -23,62 +23,63 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-// app.get('/getAll', (req, res) => {
-//   databaseHandler.getAllUsers((err, result) => {
-//     if (err) return console.log(err);
-//     res.render('viewInfo.ejs', { user_info: result });
-//   });
-// });
+app.get('/getAll', (req, res) => {
+  databaseHandler.getAllUsers((err, result) => {
+    if (err) return console.log(err);
+    res.render('viewInfo.ejs', { user_info: result });
+  });
+});
 
-// // do a search by forename
-// app.get('/search', (req, res) => {
-// // having _ as a default value being passed into the sql to say  Any single character
-//   let params = {
-//     forename: req.query.forename.toLowerCase() || '_',
-//     surename: req.query.surename.toLowerCase() || '_'
-//   };
-//   databaseHandler.search(params, (err, result) => {
-//     if (err) return console.log(err);
-//     res.render('viewInfo.ejs', { user_info: result });
-//   });
-// });
+// do a search by forename
+app.get('/search', (req, res) => {
+// having _ as a default value being passed into the sql to say  Any single character
+  let params = {
+    forename: req.query.forename.toLowerCase() || '_',
+    surename: req.query.surename.toLowerCase() || '_'
+  };
+  databaseHandler.search(params, (err, result) => {
+    if (err) return console.log(err);
+    res.render('viewInfo.ejs', { user_info: result });
+  });
+});
 
-// app.post('/insert', (req, res) => {
-//   let params = {
-//     forename: req.body.first_name.toLowerCase(),
-//     surename: req.body.last_name.toLowerCase(),
-//     email: req.body.email.toLowerCase()
-//   };
-//   databaseHandler.insertUser(params, (err, result) => {
-//     if (err) return console.log(err);
-//     res.redirect('/');
-//   });
-// });
+app.post('/insert', (req, res) => {
+  let params = {
+    forename: req.body.first_name.toLowerCase(),
+    surename: req.body.last_name.toLowerCase(),
+    email: req.body.email.toLowerCase()
+  };
+  databaseHandler.insertUser(params, (err, result) => {
+    if (err) return console.log(err);
+    res.redirect('/');
+  });
+});
 
-// // had to do the update via post as put didn't want to work
-// app.post('/update', (req, res) => {
-//   let paramsToSearch = {
-//     forename: req.body.search_user_first_name.toLowerCase(),
-//     surename: req.body.search_user_last_name.toLowerCase()
-//   };
-//   let paramsToUpdate = {
-//     forename: req.body.update_user_first_name.toLowerCase(),
-//     surename: req.body.update_user_last_name.toLowerCase(),
-//     email: req.body.update_user_email.toLowerCase()
-//   };
-//   databaseHandler.updateUser(paramsToUpdate, paramsToSearch, (err, result) => {
-//     if (err) return console.log(err);
-//   });
-// });
+// had to do the update via post as put didn't want to work
+app.post('/update', (req, res) => {
+  let paramsToSearch = {
+    forename: req.body.search_user_first_name.toLowerCase(),
+    surename: req.body.search_user_last_name.toLowerCase()
+  };
+  let paramsToUpdate = {
+    forename: req.body.update_user_first_name.toLowerCase(),
+    surename: req.body.update_user_last_name.toLowerCase(),
+    email: req.body.update_user_email.toLowerCase()
+  };
+  databaseHandler.updateUser(paramsToUpdate, paramsToSearch, (err, result) => {
+    if (err) return console.log(err);
+    return res.redirect('/');
+  });
+});
 
-// // had to do the delete via post as doing app.delete didn't want to work
-// app.post('/delete', (req, res) => {
-//   let params = {
-//     forename: req.body.first_name.toLowerCase(),
-//     surename: req.body.last_name.toLowerCase()
-//   };
-//   databaseHandler.deleteUser(params, (err, result) => {
-//     if (err) return console.log(err);
-//     return res.redirect('/');
-//   });
-// });
+// had to do the delete via post as doing app.delete didn't want to work
+app.post('/delete', (req, res) => {
+  let params = {
+    forename: req.body.first_name.toLowerCase(),
+    surename: req.body.last_name.toLowerCase()
+  };
+  databaseHandler.deleteUser(params, (err, result) => {
+    if (err) return console.log(err);
+    return res.redirect('/');
+  });
+});
