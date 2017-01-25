d52804b7e663f45fe7fc74ef6d926ae5afab078f
Jordan Hood <jordan.hood@holidayextras.com>
2017-01-25 21:16:46 +0000

    error handling

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,50 +1,76 @@
 'use strict';
-
+var Q = require('q');
 var models = require('../models/');
 var UsersController = {};
+var { isEmpty } = require('lodash');
+
 
 UsersController.getAll = function() {
-  return models.user.findAll({ raw: true })
-    .then(function(users) {
-      if (users.length) {
-        return users;
-      }
-      return 'Could not find any users.';
-    })
-    .catch(function(error) {
-      return 'Error retrieving users.' + error;
-    });
+  var deferred = Q.defer();
+  models.user.findAll({
+    raw: true
+  })
+  .then(function(users) {
+    if (isEmpty(users)) {
+      return deferred.reject( new Error('Could not find any users'));
+    }
+    return deferred.resolve(users);
+  })
+  .catch(function(error) {
+    return deferred.reject(new Error('Error retrieving users.' + error));
+  });
+  return deferred.promise;
 };
 
 UsersController.get = function(id) {
-  return models.user.find({
+  var deferred = Q.defer();
+  models.user.find({
     raw: true,
     where: {
       id: id
     }
   }).then(function(user) {
-    return user;
+    return deferred.resolve(user);
   })
   .catch(function(error) {
-    return 'error retrieving user.' + error;
+    return deferred.reject('error retrieving user.' + error);
   });
+  return deferred.promise;
 };
 
-UsersController.update = function() {
+UsersController.update = function(id, data) {
+  var deferred = Q.defer();
+  models.user.update(data, {
+    where: {
+      id: id
+    },
+    returning: true
+  }).then(function(result) {
+    return deferred.resolve(result);
+  })
+  .catch(function(err) {
+    return deferred.reject(err);
+  });
+  return deferred.promise;
 };
 
 UsersController.delete = function(id) {
+  var deferred = Q.defer();
   models.user.destroy({
     where: {
       id: id
     }
-  }).then(function() {
-      //do nothing
+  }).then(function(result) {
+    return deferred.resolve(result);
+  })
+  .catch(function(err) {
+    return deferred.reject(err);
   });
+  return deferred.promise;
 };
 
 UsersController.create = function(data) {
-
+  var deferred = Q.defer();
   var userDetails = {
     email: data.email,
     forname: data.forname,
@@ -54,7 +80,11 @@ UsersController.create = function(data) {
 
   models.user.create(userDetails)
   .then(function(user) {
-    return user;
+    return deferred.resolve(user);
+  })
+  .catch(function(err) {
+    deferred.reject(err);
   });
+  return deferred.promise;
 };
 module.exports = UsersController;
