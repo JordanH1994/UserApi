7b09f7e5d49dbc24bfe1e3bca3abb2bab00e693f
Jordan Hood <jordan.hood@holidayextras.com>
2017-01-25 21:17:40 +0000

    remove redundant files

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 'use strict';
 var app = require('express')();
-var path = require('path');
-// var favicon = require('serve-favicon');
 var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var models = require('./models');
-var index = require('./routes/index');
 var users = require('./routes/users');
 var config = require('./config/config')();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(multer());
 
-app.use(require('express').static(path.join(__dirname, 'public')));
-
-app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
