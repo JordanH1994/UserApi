d86d7ab8e73779e51ca6f838c3dac8d5b65f4f59
Jordan Hood <jordan.hood@holidayextras.com>
2017-02-03 17:31:54 +0000

    small update to start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,9 @@ psql -tAc "SELECT 1 FROM pg_database WHERE datname='api'" | grep -q 1 || psql -c
 #dump the sql file into the database 
 psql -q -U api -d api -1 -f ./scripts/.setupDatabase.sql &&
 
-nodemon app.js development
+S1=$1
+if [ -z "$S1" ];
+then
+				S1='developent'
+fi
+nodemon app.js $S1
