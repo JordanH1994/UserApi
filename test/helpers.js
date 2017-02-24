bc39571cf7c07dae0f90b7bd9271c2c0f836e441
Jordan Hood <jordan.hood@holidayextras.com>
2017-02-24 15:36:35 +0000

    remove unused dependancy

--- test/helpers.js
+++ test/helpers.js
@@ -5,7 +5,6 @@ global.expect = chai
   .use(require('chai-as-promised'))
   .use(require('dirty-chai'))
   .use(require('sinon-chai'))
-  .use(require('chai-http'))
   .expect
 global.chai = chai
 global.should = chai.should()
