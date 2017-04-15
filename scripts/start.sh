234f33147d35e995bbc7480817ff3674a37118ca
Jordan Hood <jordan.hood@holidayextras.com>
2017-04-15 13:28:32 +0100

    sql to json

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,6 @@
 psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='api'" | grep -q 1 || psql -c "CREATE USER api WITH PASSWORD 'api';" && 
 #create a temp database if the database doesn't exist
 psql -tAc "SELECT 1 FROM pg_database WHERE datname='api'" | grep -q 1 || psql -c "CREATE DATABASE api;" && 
-#dump the sql file into the database 
-psql -q -U api -d api -1 -f ./scripts/.setupDatabase.sql &&
 
 NODE_ENV=development DATABASE_URL=postgres://api:api@localhost:5432/api nodemon app.js
 
