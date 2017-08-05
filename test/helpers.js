9875a8027c19056d650cba779f54c74f20822f69
Jordan Hood <jordan.hood@holidayextras.com>
2017-08-05 18:40:22 +0100

    Revert "test break"
    This reverts commit 909b8c77b681c6f1a85b5cd02700028d6fb98b38.

--- test/helpers.js
+++ test/helpers.js
@@ -1,13 +1,13 @@
-// 'use strict'
+'use strict'
 
-// const chai = require('chai')
-// global.expect = chai
-//   .use(require('chai-as-promised'))
-//   .use(require('dirty-chai'))
-//   .use(require('sinon-chai'))
-//   .expect
-// global.chai = chai
-// global.should = chai.should()
-// global.sinon = require('sinon')
-// global.sandbox = sinon.sandbox.create()
-// process.env.DATABASE_URL = 'postgres://api:api@localhost:5432/api'
+const chai = require('chai')
+global.expect = chai
+  .use(require('chai-as-promised'))
+  .use(require('dirty-chai'))
+  .use(require('sinon-chai'))
+  .expect
+global.chai = chai
+global.should = chai.should()
+global.sinon = require('sinon')
+global.sandbox = sinon.sandbox.create()
+process.env.DATABASE_URL = 'postgres://api:api@localhost:5432/api'
